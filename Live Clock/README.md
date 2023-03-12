ad02a3e4693b690afe3cc7196e2d72e89e7ce23b
kgalip <125612543+kgalip@users.noreply.github.com>
2023-03-12 17:01:30 +0300

    Add files via upload

---
+++ Live Clock/README.md
@@ -0,0 +1,2 @@
+My third project using JavaScript.
+I got help from ChatGPT but it made me learn the Date and setInterval functions very well.
---
+++ Live Clock/js/main.js
@@ -0,0 +1,27 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+
+function atTheMoment() {
+    let now = new Date();
+
+    let currentHour = now.getHours();
+    let currentMinute = now.getMinutes();
+    let currentSecond = now.getSeconds();
+
+    if (currentHour < 10) {
+        currentHour = "0" + currentHour;
+    };
+    if (currentMinute < 10) {
+        currentMinute = "0" + currentMinute;
+    };
+    if (currentSecond < 10) {
+        currentSecond = "0" + currentSecond;
+    };
+
+    hour.textContent = currentHour;
+    minute.textContent = currentMinute;
+    second.textContent = currentSecond;
+};
+
+setInterval(atTheMoment, 200);
